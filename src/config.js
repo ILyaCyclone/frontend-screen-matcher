65292588b3f49668c62dd823bbeadb5318a4ed3b
ILya <ilya.cyclone@gmail.com>
2019-08-15 20:03:57 +0300

    update config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const resolutions = {
     "375": {
         description: "xs - mobile 1 column",
         width: 375
-    }, 
+    },
     "576": {
         description: "sm - mobile 2 column",
         width: 576
@@ -29,81 +29,109 @@ const resolutions = {
     }
 };
 
-const baseUrl = "https://rut-miit.ru";
-// const baseUrl = "http://localhost";
+const baseUrl =
+    "https://rut-miit.ru";
+    // "http://10.242.101.40";
+
+    // "http://wl3n1.miit.ru:7003/portal";
+    // "http://wl3n2.miit.ru:7003/portal";
 
 const addresses = {
     "main": {
-        description : "home page",
+        description: "home page",
         address: baseUrl
         , ignores: [
-            {type: "css", selector: ".main-slider-v2", description: "slide may change during screenshot making"},
+            { type: "css", selector: ".main-slider-v2", description: "slide may change during screenshot making" },
             // {type: "css", selector: ".article-list__preview-wrap", description: "new list"}
-            {type: "css", selector: ".article__preview > a", description: "each article card"}
+            { type: "css", selector: ".article__preview > a", description: "each article card" }
         ]
     },
-    "news": {address: baseUrl+"/news"
-        , waits: [{type: "css", selector: ".article-list .article__preview"}]
+    "news": {
+        address: baseUrl + "/news"
+        , waits: [{ type: "css", selector: ".article-list .article__preview" }]
         , ignores: [
             // {type: "css", selector: "#articles", description: "new list"}
-            {type: "css", selector: ".article__preview > a", description: "each article card"}
+            { type: "css", selector: ".article__preview > a", description: "each article card" }
         ]
     },
-    "admissions": {address: baseUrl+"/admissions"
-        , waits: [{type: "css", selector: ".na-science-container .na-science"}]
+    "admissions": {
+        address: baseUrl + "/admissions"
+        , ignores: [{ type: "css", selector: "#yandex-map" }]
+        , waits: [{ type: "css", selector: ".na-science-container .na-science" }]
     },
-    "admissions-degrees": {address: baseUrl+"/admissions/degrees"
-        , waits: [{type: "css", selector: ".applicants_specialization .applicants__places"}]
+    "admissions-degrees": {
+        address: baseUrl + "/admissions/degrees"
+        , waits: [{ type: "css", selector: ".applicants_specialization .applicants__places" }]
         , ignores: [
-            {type: "css", selector: ".applicants_specialization"}
+            { type: "css", selector: ".applicants_specialization" }
         ]
     },
-    "admissions-degrees-rating": {address: baseUrl+"/admissions/degrees/51259"
-        , waits: [{type: "css", selector: ".applicants__list-table-wrap.applicants__list-table-wrap--bottom"}]
+    "admissions-degrees-rating": {
+        address: baseUrl + "/admissions/degrees/51259"
+        , waits: [{ type: "css", selector: ".applicants__list-table-wrap.applicants__list-table-wrap--bottom" }]
         , ignores: [
-            {type: "css", selector: ".js-applicants.glasspane__wrapper"}
+            { type: "css", selector: ".applicants__places-xs", description: "number of admissions mobile" },
+            { type: "css", selector: ".applicants__places tbody", description: "number of admissions desktop" },
+            { type: "css", selector: ".js-applicants.glasspane__wrapper" }
         ]
     },
 
-    "timetable": {address: baseUrl+"/timetable/141448"},
-    "timetable-catalog": {address: baseUrl+"/timetable"},
+    "timetable-catalog": { address: baseUrl + "/timetable" },
+    "timetable-group": { address: baseUrl + "/timetable/140048" },
+    "timetable-profile": { address: baseUrl + "/people/709306/timetable" },
+    "timetable-group-exams": { address: baseUrl + "/timetable/140048?start=2020-01-01", description: "exams" },
 
-    "news-item_with-wide-image": {address: baseUrl+"/news/159935"},
-    "news-item_with-small-image": {address: baseUrl+"/news/162380"},
-    "news-item_without-image": {address: baseUrl+"/news/159825"},
-    
-    "article_with-side-menu": {address: baseUrl+"/sveden/education"},
-    "article_without-side-menu": {address: baseUrl+"/sveden"},
-    "article_with-list": {address: baseUrl+"/org/projects/lectures"},
-    
-    "depts": {address: baseUrl+"/depts"},
-    "dept": {address: baseUrl+"/depts/294"},
-    
-    "people": {address: baseUrl+"/people"},
-    "profile": {address: baseUrl+"/people/17"},
+    "news-item_with-wide-image": { address: baseUrl + "/news/159935" },
+    "news-item_with-small-image": { address: baseUrl + "/news/162380" },
+    "news-item_without-image": { address: baseUrl + "/news/159825" },
 
-    "edu-programs": {address: baseUrl+"/edu/programs"},
-    "speciality": {address: baseUrl+"/edu/programs/1013100"},
-    "speciality-implementation": {address: baseUrl+"/edu/programs/1013100/4267916"},
+    "article_with-side-menu": { address: baseUrl + "/sveden/education" },
+    "article_without-side-menu": { address: baseUrl + "/sveden" },
+    "article_with-list": { address: baseUrl + "/org/projects/lectures" },
 
-    "feedback": {address: baseUrl+"/feedback"},
+    "depts": { address: baseUrl + "/depts" },
+    "dept": { address: baseUrl + "/depts/294" },
+
+    "people": {
+        address: baseUrl + "/people"
+        , ignores: [{ type: "css", selector: ".people .info-block__header-text .text-secondary", description: "number of people" }]
+    },
+    "profile": { address: baseUrl + "/people/17" },
+
+    "edu-programs": { address: baseUrl + "/edu/programs" },
+    "speciality": {
+        address: baseUrl + "/edu/programs/1013100"
+        , ignores: [
+            { type: "css", selector: ".applicants__props-wrap_md-place", description: "number of admissions mobile" },
+            { type: "css", selector: ".applicants__places table table", description: "number of admissions desktop" }
+        ]
+    },
+    "speciality-implementation": {
+        address: baseUrl + "/edu/programs/1013100/4267916"
+        , ignores: [
+            { type: "css", selector: ".applicants__props-wrap_md-place", description: "number of admissions mobile" },
+            { type: "css", selector: ".applicants__places table table", description: "number of admissions desktop" }
+        ]
+    },
+
+    "feedback": { address: baseUrl + "/feedback" },
 };
 
 const directories = {
     "test": {
-        description : "directory for test screenshots",
+        description: "directory for test screenshots",
         path: '.\\screenshots\\test'
     },
     "golden": {
-        description : "directory for golden screenshots",
+        description: "directory for golden screenshots",
         path: '.\\screenshots\\golden'
     },
     "result": {
-        description : "directory for result pictures",
+        description: "directory for result pictures",
         path: '.\\screenshots\\results'
     }
 };
 
 module.exports.resolutions = resolutions;
 module.exports.addresses = addresses;
-module.exports.directories  = directories ;
+module.exports.directories = directories;
